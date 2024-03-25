8baa96d1700b87d86176651dba3e4bb21a1eec38
Alexandr <61061837+AlexandrVanchukov@users.noreply.github.com>
2024-03-25 19:54:14 +0300

    Add get and send data with server

--- js/form.js
+++ js/form.js
@@ -2,11 +2,15 @@ import { isEscapeKey } from './util.js';
 import '/vendor/pristine/pristine.min';
 import {percentToFloat} from './util.js';
 import '/vendor/nouislider/nouislider.js';
+import { showAlert } from './util.js';
+import { sendData } from './api.js';
+import { showSuccess } from './util.js';
 
 const imgInput = document.querySelector('.img-upload__input');
 const imgOverlay = document.querySelector('.img-upload__overlay');
 const imgUploadForm = document.querySelector('.img-upload__form');
 const imgPreview = document.querySelector('.img-upload__preview>img');
+const closeButton = imgOverlay.querySelector('.img-upload__cancel');
 
 const hashtagsInput = document.querySelector('.text__hashtags');
 const descriptionInput = document.querySelector('.text__description');
@@ -36,6 +40,11 @@ noUiSlider.create(effectSliderInput,{
   connect: 'lower',
 });
 
+const SubmitButtonText = {
+  IDLE: 'Сохранить',
+  SENDING: 'Сохраняю...'
+};
+
 let filterName = '';
 let filterUnit = '';
 effectSliderInput.noUiSlider.on('update', () => {
@@ -59,150 +68,151 @@ imgInput.addEventListener('change',(evt)=>{
     imgPreviewEffect.style.backgroundImage = `url(${ downloadedImg })`;
   });
 
-  //изменение размера изображения
-  function smallerHandler(){
-    let scale = percentToFloat(size.value);
-    if (scale > MIN_SCALE) {
-      scale = scale - SCALE_STEP;
-      size.value = `${scale * 100}%`;
-      imgPreview.style.transform = `scale(${ scale })`;
-    }
-  }
-  function biggerHandler(){
-    let scale = percentToFloat(size.value);
-    if (scale < MAX_SCALE) {
-      scale = scale + SCALE_STEP;
-      size.value = `${scale * 100}%`;
-      imgPreview.style.transform = `scale(${ scale })`;
-    }
-  }
+  //изменение размера изображения и применение эффектов
   smallerButton.addEventListener('click', smallerHandler);
   biggerButton.addEventListener('click', biggerHandler);
-
-  //Приминение фильтров
-  function effectsHandler(){
-    const effectName = effectsList.querySelector('input:checked').value;
-    effectSlider.classList.remove('hidden');
-
-    switch (effectName){
-      case 'none':
-        effectSliderValue.value = 0;
-        imgPreview.style.filter = 'none';
-        effectSlider.classList.add('hidden');
-        break;
-      case 'chrome':
-        filterName = 'grayscale';
-        filterUnit = '';
-        effectSliderInput.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 1
-          },
-          start: 1,
-          step: 0.1,
-        });
-        effectSliderInput.noUiSlider.set(1);
-        break;
-      case 'sepia':
-        filterName = 'sepia';
-        filterUnit = '';
-        effectSliderInput.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 1
-          },
-          start: 1,
-          step: 0.1
-        });
-        effectSliderInput.noUiSlider.set(1);
-        break;
-      case 'marvin':
-        filterName = 'invert';
-        filterUnit = '%';
-        effectSliderInput.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 100
-          },
-          start: 100,
-          step: 1
-        });
-        effectSliderInput.noUiSlider.set(100);
-        break;
-      case 'phobos':
-        filterName = 'blur';
-        filterUnit = 'px';
-        effectSliderInput.noUiSlider.updateOptions({
-          range: {
-            min: 0,
-            max: 3
-          },
-          start: 3,
-          step: 0.1
-        });
-        effectSliderInput.noUiSlider.set(3);
-        break;
-      case 'heat':
-        filterName = 'brightness';
-        filterUnit = '';
-        effectSliderInput.noUiSlider.updateOptions({
-          range: {
-            min: 1,
-            max: 3
-          },
-          start: 3,
-          step: 0.1
-        });
-        effectSliderInput.noUiSlider.set(3);
-        break;
-    }
-    // imgPreview.style.filter = 'grayscale(1)';
-  }
   effectsList.addEventListener('change', effectsHandler);
 
   //добавление listeners на закрытие модульного окна
-  const closeButton = imgOverlay.querySelector('.img-upload__cancel');
   closeButton.addEventListener('click', closeImgOverlay);
   document.addEventListener('keydown', closeByKey);
 
   //при фокусе на интпутах отключаем закритие на Ecs
   disableKeyCloseByInputOnfocus(hashtagsInput);
   disableKeyCloseByInputOnfocus(descriptionInput);
-
-  function closeImgOverlay(){
-    imgOverlay.classList.add('hidden');
-    document.body.classList.remove('modal-open');
-
-    closeButton.removeEventListener('click', closeImgOverlay);
-    document.removeEventListener('keydown', closeByKey);
-
-    smallerButton.removeEventListener('click', smallerHandler);
-    biggerButton.removeEventListener('click', biggerHandler);
-
-    effectsList.removeEventListener('change', effectsHandler);
-    effectSliderValue.value = 0;
-    effectSliderInput.noUiSlider.set(0);
-    imgPreview.style.filter = 'none';
-
-    imgUploadForm.reset();
-  }
-
-  function closeByKey(e){
-    if(isEscapeKey(e)){
-      evt.preventDefault();
-      closeImgOverlay();
-    }
-  }
-
-  function disableKeyCloseByInputOnfocus(input){
-    input.onfocus = ()=>{
-      document.removeEventListener('keydown', closeByKey);
-    };
-    input.onblur = ()=>{
-      document.addEventListener('keydown', closeByKey);
-    };
-  }
 });
+
+function smallerHandler(){
+  let scale = percentToFloat(size.value);
+  if (scale > MIN_SCALE) {
+    scale = scale - SCALE_STEP;
+    size.value = `${scale * 100}%`;
+    imgPreview.style.transform = `scale(${ scale })`;
+  }
+}
+function biggerHandler(){
+  let scale = percentToFloat(size.value);
+  if (scale < MAX_SCALE) {
+    scale = scale + SCALE_STEP;
+    size.value = `${scale * 100}%`;
+    imgPreview.style.transform = `scale(${ scale })`;
+  }
+}
+
+//Приминение фильтров
+function effectsHandler(){
+  const effectName = effectsList.querySelector('input:checked').value;
+  effectSlider.classList.remove('hidden');
+
+  switch (effectName){
+    case 'none':
+      effectSliderValue.value = 0;
+      imgPreview.style.filter = 'none';
+      effectSlider.classList.add('hidden');
+      break;
+    case 'chrome':
+      filterName = 'grayscale';
+      filterUnit = '';
+      effectSliderInput.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 1
+        },
+        start: 1,
+        step: 0.1,
+      });
+      effectSliderInput.noUiSlider.set(1);
+      break;
+    case 'sepia':
+      filterName = 'sepia';
+      filterUnit = '';
+      effectSliderInput.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 1
+        },
+        start: 1,
+        step: 0.1
+      });
+      effectSliderInput.noUiSlider.set(1);
+      break;
+    case 'marvin':
+      filterName = 'invert';
+      filterUnit = '%';
+      effectSliderInput.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 100
+        },
+        start: 100,
+        step: 1
+      });
+      effectSliderInput.noUiSlider.set(100);
+      break;
+    case 'phobos':
+      filterName = 'blur';
+      filterUnit = 'px';
+      effectSliderInput.noUiSlider.updateOptions({
+        range: {
+          min: 0,
+          max: 3
+        },
+        start: 3,
+        step: 0.1
+      });
+      effectSliderInput.noUiSlider.set(3);
+      break;
+    case 'heat':
+      filterName = 'brightness';
+      filterUnit = '';
+      effectSliderInput.noUiSlider.updateOptions({
+        range: {
+          min: 1,
+          max: 3
+        },
+        start: 3,
+        step: 0.1
+      });
+      effectSliderInput.noUiSlider.set(3);
+      break;
+  }
+  // imgPreview.style.filter = 'grayscale(1)';
+}
+
+function closeImgOverlay(){
+  imgOverlay.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+
+  closeButton.removeEventListener('click', closeImgOverlay);
+  document.removeEventListener('keydown', closeByKey);
+
+  smallerButton.removeEventListener('click', smallerHandler);
+  biggerButton.removeEventListener('click', biggerHandler);
+
+  effectsList.removeEventListener('change', effectsHandler);
+  effectSliderValue.value = 0;
+  effectSliderInput.noUiSlider.set(0);
+  imgPreview.style.filter = 'none';
+
+  imgUploadForm.reset();
+}
+
+function closeByKey(e){
+  if(isEscapeKey(e)){
+    e.preventDefault();
+    closeImgOverlay();
+  }
+}
+
+function disableKeyCloseByInputOnfocus(input){
+  input.onfocus = ()=>{
+    document.removeEventListener('keydown', closeByKey);
+  };
+  input.onblur = ()=>{
+    document.addEventListener('keydown', closeByKey);
+  };
+}
+
 
 const pristine = new Pristine(imgUploadForm);
 
@@ -300,10 +310,37 @@ function validateDescription(description){
 pristine.addValidator(hashtagsInput, validateHashTags);
 pristine.addValidator(descriptionInput, validateDescription);
 
+const uploadSubmitButton = document.querySelector('.img-upload__submit');
+
+const blockSubmitButton = () => {
+  uploadSubmitButton.disabled = true;
+  uploadSubmitButton.textContent = SubmitButtonText.SENDING;
+};
+
+const unblockSubmitButton = () => {
+  uploadSubmitButton.disabled = false;
+  uploadSubmitButton.textContent = SubmitButtonText.IDLE;
+};
+
+
+// const setUserFromSubmit = (onSuccess) => {
 imgUploadForm.addEventListener('submit',(evt)=>{
   evt.preventDefault();
-  if(pristine.validate()){
+
+  const isValid = pristine.validate();
+  if(isValid){
     // console.log("Форма успошно отправлена!");
-    imgUploadForm.submit();
+    const formData = new FormData(evt.target);
+    blockSubmitButton();
+    // imgUploadForm.submit();
+    sendData(formData)
+      .then(closeImgOverlay)
+      .then(showSuccess)
+      .catch((err)=>{
+        showAlert(err.message);
+      })
+      .finally(unblockSubmitButton);
   }
 });
+// };
+
--- js/main.js
+++ js/main.js
@@ -1,2 +1,14 @@
-import './gallery.js';
+import './renderPost.js';
 import './form.js';
+import { getData } from './api.js';
+import { showAlert } from './util.js';
+import {renderPosts} from './renderPost.js';
+
+getData()
+  .then((wizards) => {
+    renderPosts(wizards);
+  })
+  .catch((err)=>{
+    showAlert(err.message);
+  });
+
